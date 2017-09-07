1cd5b005fb0c8e55878770377e6c3aef54b95f64
Jacob Brunette <jacob@dhcp-206-87-154-174.ubcsecure.wireless.ubc.ca>
2017-09-07 15:35:33 -0700

    sql experiments + rudimentary flask framework

---
+++ src/experiments/create-table-test.py
@@ -0,0 +1,38 @@
+import psycopg2
+
+def create_table():
+    commands = (
+        """
+        CREATE TYPE admin_level AS ENUM ('user', 'admin', 'superadmin')
+        """,
+        """
+        CREATE TYPE unit AS ENUM ('exampleUnit1', 'exampleUnit2')
+        """,
+        """
+        CREATE TABLE users (
+          user_id SERIAL PRIMARY KEY,
+          username TEXT NOT NULL,
+          unit unit,
+          admin_level admin_level NOT NULL
+        )
+        """
+    )
+
+    conn = None
+
+    try:
+        conn = psycopg2.connect(host="localhost", database="sustainabilitytest", user="postgres", password="SuperUser100")
+        cur = conn.cursor()
+        for command in commands:
+            cur.execute(command)
+
+        cur.close()
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+if __name__ == '__main__':
+    create_table()
